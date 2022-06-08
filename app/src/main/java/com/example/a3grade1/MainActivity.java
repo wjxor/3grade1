0eca33323a733c03ba8c533be61b1dd3f32dc22b
wjxor <shko940575@gmail.com>
2022-06-09 03:59:44 +0900

    사진보기 앱

--- app/src/main/java/com/example/a3grade1/MainActivity.java
+++ app/src/main/java/com/example/a3grade1/MainActivity.java
@@ -2,23 +2,51 @@ package com.example.a3grade1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageViewTop;
+
+    private static final int REQUEST_CODE_PICK_IMAGE = 3000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("메인화면");
+        setTitle("사진보는 앱");
 
-        int loginedMemberId = getIntent().getIntExtra("loginedMemberId", 0);
+        imageViewTop = findViewById(R.id.imageViewTop);
 
-        Member loginedMember = AppDatabase.findMember(loginedMemberId);
-
-        TextView textViewLoginedMemberName = findViewById(R.id.textViewLoginedMemberName);
-        textViewLoginedMemberName.setText(loginedMember.getName() + "님 환영합니다.");
     }
+
+    public void buttonPickImageClicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+            Uri selectedImageUri = data.getData();
+
+            imageViewTop.setImageURI(selectedImageUri);
+        }
+    }
+
 }
